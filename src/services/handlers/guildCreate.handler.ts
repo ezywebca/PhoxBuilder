15d25fa11beba630c463c0268e0c4c13b9fafd67
Glitch (impossible-mud-wound) <moran.storm.5050@gmail.com>
2020-12-14 22:27:46 -0500

    dsada

--- src/services/handlers/guildCreate.handler.ts
+++ src/services/handlers/guildCreate.handler.ts
@@ -9,6 +9,6 @@ export default class GuildCreateHandler implements EventHandler {
     }
 
     private sendWelcomeMessage(channel: TextChannel | null) {
-        return channel?.send(`Thanks For Inviting Me Im NewBot! Check me Out @ http://newbot.onthewifi.com`);
+        return channel?.send(`Thanks For Inviting Me Im NewBot#4779 Goto http://newbot.onthewifi.com to find out more about me and my services/commands! Also if you just invited me to the server and do not have allow dms on the bot will not be able to explain to you how to use it and you will need to use the website instead!`);
     }    
 }
